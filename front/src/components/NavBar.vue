3b19197f7dd306b807ba800580987357ae6edfae
yoonhocho <yoonhocho91@gmail.com>
2021-09-26 22:53:37 +0900

    nav footer css

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,30 +1,36 @@
 <template>
-    <ul>
-        <router-link to='/' class="logo">
-            <img src="../assets/arsenal_logo.png" alt="" width="100px">
-        </router-link>
-        <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
-            <router-link to='/team' >Team</router-link>
-            <ul v-show="showBox" class="depth" >
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-            </ul>
-        </li>
-        <li><router-link to='/history'>History</router-link></li>
-        <li><router-link to='/schedule'>Schedule</router-link></li>
-        <li><router-link to='/board'>Board</router-link></li>
-
-        <li><router-link to='/logIn'>Login</router-link></li>
-        <li><router-link to='/register'>SignUp</router-link></li>
-        <div v-show="showBox" class="depth">
-            <router-link to='/squad'>squad</router-link>
-            <router-link to='/stats'>stats</router-link>
+    <div class="nav">
+        <div class="logoWrap">
+            <a class="logo">
+                <router-link to='/'>
+                    <img src="../assets/arsenal_logo.png" alt="" width="100px">
+                </router-link>
+            </a>
         </div>
-    </ul>
+        <ul class="menu">
+            <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
+                <router-link to='/team' >Team</router-link>
+                <!-- <ul v-show="showBox" class="depth">
+                    <li><a href="">Squad</a></li>
+                    <li><a href="">Stat</a></li>
+                </ul> -->
+            </li>
+            <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
+                <router-link to='/history'>History</router-link>
+                <!-- <ul v-show="showBox" class="depth">
+                    <li><a href="">Introduce</a></li>
+                    <li><a href="">Achievement</a></li>
+                    <li><a href="">Honor of Fame</a></li>
+                </ul> -->
+            </li>
+            <li><router-link to='/schedule'>Schedule</router-link></li>
+            <li><router-link to='/board'>Board</router-link></li>
+        </ul>
+        <ul class="memberCont">
+            <li><router-link to='/logIn'>Login</router-link></li>
+            <li><router-link to='/register'>SignUp</router-link></li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -48,9 +54,13 @@ export default {
 </script>
 
 <style scoped>
-    ul {overflow: hidden; width: 100%; background: #aaa; position: fixed;top: 0;left: 0; z-index: 999;}
-    .logo {float: left; margin: 0 50px;}
-    ul > li {float: left; margin: 50px 0 0 100px;}
-    .depth {display: none;}
-    
+    .nav {overflow: hidden; background: #000; position: relative;}
+    a {color: #fff; font-weight: 600;}
+    .logoWrap {position: relative;top:0;left: 100px; display: inline-block; }
+    .menu {position: absolute;top: 70px;left: 40%;}
+    .menu > li {float: left; margin: 0 40px;}
+    .menu > li > a {position: relative; text-transform: uppercase;}
+    .memberCont {position: absolute;top: 70px;right: 10%; overflow: hidden;}
+    .memberCont > li {float: left; margin: 0 20px;}
+
 </style>
