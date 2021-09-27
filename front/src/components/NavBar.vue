24bb55a6b94ba4ac61acbcc86bd9c2b292252311
yoonhocho <yoonhocho91@gmail.com>
2021-09-27 11:51:43 +0900

    nav_var css

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -8,20 +8,27 @@
             </a>
         </div>
         <ul class="menu">
-            <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
+            <!-- <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
                 <router-link to='/team' >Team</router-link>
-                <!-- <ul v-show="showBox" class="depth">
+                <ul v-show="showBox" class="depth">
                     <li><a href="">Squad</a></li>
                     <li><a href="">Stat</a></li>
-                </ul> -->
+                </ul> 
+            </li> -->
+            <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
+                <router-link to='/team'>Team</router-link>
+                <ul v-show="showBox" class="depth">
+                    <li><a href="">Squad</a></li>
+                    <li><a href="">Stat</a></li>
+                </ul> 
             </li>
             <li v-on:mouseover="doMouseOver" v-on:mouseleave="doMouseLeave">
                 <router-link to='/history'>History</router-link>
-                <!-- <ul v-show="showBox" class="depth">
+                <ul v-show="showBox" class="depth">
                     <li><a href="">Introduce</a></li>
                     <li><a href="">Achievement</a></li>
                     <li><a href="">Honor of Fame</a></li>
-                </ul> -->
+                </ul>
             </li>
             <li><router-link to='/schedule'>Schedule</router-link></li>
             <li><router-link to='/board'>Board</router-link></li>
@@ -31,6 +38,7 @@
             <li><router-link to='/register'>SignUp</router-link></li>
         </ul>
     </div>
+    
 </template>
 
 <script>
@@ -54,12 +62,14 @@ export default {
 </script>
 
 <style scoped>
-    .nav {overflow: hidden; background: #000; position: relative;}
-    a {color: #fff; font-weight: 600;}
-    .logoWrap {position: relative;top:0;left: 100px; display: inline-block; }
+    .nav {background: #000; position: relative; margin: 0 auto;}
+    a {color: #fff; font-size: 17px; font-weight: 600;}
+    .logoWrap {position: relative;top:0;left: 100px; display: inline-block;}
     .menu {position: absolute;top: 70px;left: 40%;}
-    .menu > li {float: left; margin: 0 40px;}
-    .menu > li > a {position: relative; text-transform: uppercase;}
+    .menu > li {float: left; margin: 0 40px; position: relative;}
+    .menu > li > a {float:left;}
+    .depth {width:220px; padding: 20px 20px; font-size:15px; background: #000; position: absolute; top:30px;left: 50%; transform: translateX(-50%);}
+    .depth > li {text-align: center; }
     .memberCont {position: absolute;top: 70px;right: 10%; overflow: hidden;}
     .memberCont > li {float: left; margin: 0 20px;}
 
