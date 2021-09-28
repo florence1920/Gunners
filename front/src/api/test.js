3f8753b82928b9f20f96c04812a0ad6710101e5a
florence1920 <jetaime12121@gmail.com>
2021-09-28 12:16:37 +0900

    1

---
+++ front/src/api/test.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+function getMember(){
+    const url = 'http://localhost:3000/'
+    return axios.get(url)
+}
+
+export {getMember}
--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -5,8 +5,11 @@
 </template>
 
 <script>
+import {getMember} from '@/api/test.js'
 export default {
-
+    mounted(){
+        getMember()
+    }
 }
 </script>
 
