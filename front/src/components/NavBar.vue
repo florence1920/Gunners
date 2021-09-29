699fa6987098fab7ad7709f82dc1f579efeaf355
yoonhocho <yoonhocho91@gmail.com>
2021-09-29 11:17:57 +0900

    csss

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -34,8 +34,12 @@
             <li><router-link to='/board'>Board</router-link></li>
         </ul>
         <ul class="memberCont">
-            <li><router-link to='/logIn'>Login</router-link></li>
-            <li><router-link to='/register'>SignUp</router-link></li>
+            <li><router-link to='/logIn'>
+                <img src="../assets/logIn.png" alt="" width="29px">
+            </router-link></li>
+            <li><router-link to='/register'>
+                <img src="../assets/signUp.png" alt="" width="27px">
+            </router-link></li>
         </ul>
     </div>
     
@@ -46,7 +50,7 @@ export default {
     data : function(){
         return{
             showBox : [
-              {show : false}, { show : false}  
+                {show : false}, { show : false}  
             ],
         }
     
@@ -66,15 +70,15 @@ export default {
 </script>
 
 <style scoped>
-    .nav {background: #000; position: relative; margin: 0 auto;}
-    a {color: #fff; font-size: 17px; font-weight: 600;}
+    .nav {background: #fff; border-bottom: 1px solid #dadada; position: relative; margin: 0 auto;}
+    a {color: #000; font-size: 17px; font-weight: 600;}
     .logoWrap {position: relative;top:0;left: 100px; display: inline-block;}
-    .menu {position: absolute;top: 70px;left: 40%;}
-    .menu > li {float: left; margin: 0 40px; position: relative;}
+    .menu {position: absolute;top: 70px;left: 30%;}
+    .menu > li {float: left; margin: 0 30px; position: relative;}
     .menu > li > a {float:left;}
-    .depth {width:220px; padding: 20px 20px; font-size:15px; background: #000; position: absolute; top:30px;left: 50%; transform: translateX(-50%);}
+    .depth {width:220px; padding: 20px 20px; font-size:15px; background: #fff; position: absolute; top:30px;left: 50%; transform: translateX(-50%);}
     .depth > li {text-align: center; }
-    .memberCont {position: absolute;top: 70px;right: 10%; overflow: hidden;}
+    .memberCont {position: absolute;top: 70px;right: 20%; overflow: hidden;}
     .memberCont > li {float: left; margin: 0 20px;}
 
 </style>
--- front/src/components/Weather.vue
+++ front/src/components/Weather.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-      서울 날씨 ^^
-      {{this.$store.state.weather.data.weather[0].main}}
-  </div>
+    <div>
+        서울 날씨 ^^
+        {{this.$store.state.weather.data.weather[0].main}}
+    </div>
 </template>
 
 <script>
