5f61b05e3b5964ed595f57790a3b21499d4ea5fa
yoonhocho <yoonhocho91@gmail.com>
2021-10-04 10:43:48 +0900

    matchescss

--- front/src/components/Matches.vue
+++ front/src/components/Matches.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    리그 전체 경기
+    <ul>
+      <li class="matchesDate">
+        <ul>
+          <li><p>October</p></li>
+          <li><p>18</p></li>
+          <li><p>2021</p></li>
+        </ul>
+      </li>
+      <li class="matchesInfo">
+        <ul>
+          <li class="homeTeam">
+            <p><span><img src="" alt=""></span>Arsenal</p>
+          </li>
+          <li class="stadium"></li>
+          <li class="awayTeam">
+            <p><span><img src="" alt=""></span>Crystal Palace</p>
+          </li>
+        </ul>
+      </li>
+      <li class="leagueType"></li>
+    </ul>
   </div>
 </template>
 
@@ -12,4 +32,7 @@ export default {
 
 <style scoped>
   div {background: #ccc;}
+  .matchesDate {width: 20%;}
+  .matchesInfo {width: 60%;}
+  .leagueType {width: 20%;}
 </style>
