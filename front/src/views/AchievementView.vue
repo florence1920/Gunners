1e388f07545f7dcc56d22a83c0e456843ab47e45
yoonhocho <yoonhocho91@gmail.com>
2021-10-08 10:46:42 +0900

    achievement Css

--- front/src/views/AchievementView.vue
+++ front/src/views/AchievementView.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="wrapAll">
-        Achievement
+        <p class="tit">트로피</p>
+        <ul class="trophiesWrap">
+            <li>
+                <div class="eplTrophy"></div>
+                <p class="num">18</p>
+                <p class="leagueName">프리미어리그</p>
+            </li>
+            <li>
+                <div class="faTrophy"></div>
+                <p class="num">18</p>
+                <p class="leagueName">프리미어리그</p>
+            </li>
+            <li>
+                <div class="faTrophy"></div>
+                <p class="num">18</p>
+                <p class="leagueName">프리미어리그</p>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -9,5 +26,7 @@
 </script>
 
 <style>
-
+.WrapAll {}
+.WrapAll .trophiesWrap {overflow: hidden; width: 80%; background: #aaa; margin: 0 auto;}
+.WrapAll .trophiesWrap li {float: left;}
 </style>
