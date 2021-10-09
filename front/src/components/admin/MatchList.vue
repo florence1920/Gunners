9f5af42515351833982def9326beba759016327c
smlee111 <vic_sm@naver.com>
2021-10-09 15:51:37 +0900

    admin matchlist

--- front/src/components/admin/MatchList.vue
+++ front/src/components/admin/MatchList.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-      <ul class="trow" v-for="(match, index) in this.$store.state.matches" :key="index">
-          <li>{{match.time[0]}} // {{match.time}}</li>
-          <li>{{match.opponent}}</li>
-          <li>{{match.ground}}</li>
-          <li>{{match.leagueName}}</li>
-        </ul>     
+      <div class="tableWrap matchList">
+            <p class="head">경기 일정 관리</p>
+            <ul class="thead">
+                <li>일시</li>
+                <li>상대팀</li>
+                <li>경기장</li>
+                <li>리그</li>
+            </ul>
+            <ul class="trow" v-for="(match, index) in this.$store.state.matches" :key="index">
+                <li>{{match.time[0]}} // {{match.time}}</li>
+                <li>{{match.opponent}}</li>
+                <li>{{match.ground}}</li>
+                <li>{{match.leagueName}}</li>
+            </ul>
+      </div>    
   </div>
 </template>
 
@@ -27,13 +36,13 @@ export default {
   .matchList li {text-align: center; padding: 18px 0;}
   .matchList .thead {background: #333;}
   .matchList .thead li {color:#fff;}
-  .matchList .thead li:nth-child(1) {width:35%; text-align: left; padding: 18px 40px;}
-  .matchList .thead li:nth-child(2) {width:15%;}
-  .matchList .thead li:nth-child(3) {width:35%;}
-  .matchList .thead li:nth-child(4) {width:15%;}
+  .matchList .thead li:nth-child(1) {width:30%; text-align: left; padding: 18px 40px;}
+  .matchList .thead li:nth-child(2) {width:20%;}
+  .matchList .thead li:nth-child(3) {width:40%;}
+  .matchList .thead li:nth-child(4) {width:10%;}
   .matchList .trow {background: #eee; border-bottom: 1px solid #e1e1e1;}
-  .matchList .trow li:nth-child(1) {width:35%; text-align: left; padding: 18px 40px;}
-  .matchList .trow li:nth-child(2) {width:15%;}
-  .matchList .trow li:nth-child(3) {width:35%;}
-  .matchList .trow li:nth-child(4) {width:15%;}
+  .matchList .trow li:nth-child(1) {width:30%; text-align: left; padding: 18px 40px;}
+  .matchList .trow li:nth-child(2) {width:20%;}
+  .matchList .trow li:nth-child(3) {width:40%;}
+  .matchList .trow li:nth-child(4) {width:10%;}
 </style>
