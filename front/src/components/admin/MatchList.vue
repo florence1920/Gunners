0feb079a59c20f2269f3eb65f99e82ecb4031581
florence1920 <jetaime12121@gmail.com>
2021-10-09 15:36:44 +0900

    time

--- front/src/components/admin/MatchList.vue
+++ front/src/components/admin/MatchList.vue
@@ -3,10 +3,11 @@
       <h1>matchlist</h1>
       <ul>
         <li v-for="(match, index) in this.$store.state.matches" :key="index">
-          매치 date : {{match.matchData}}/
+          매치 date : {{match.time[0]}}/
           상대방 : {{match.opponent}}/
           경기장 : {{match.ground}}/
           리그 : {{match.leagueName}}
+          남은시간 : {{match.time}}
         </li>
       </ul>
   </div>
