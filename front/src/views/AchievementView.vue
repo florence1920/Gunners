048b2f7892c16ef68fb48bc8532dc5e924f06487
yoonhocho <yoonhocho91@gmail.com>
2021-10-14 10:08:14 +0900

    Achievement CSS 추가

--- front/src/views/AchievementView.vue
+++ front/src/views/AchievementView.vue
@@ -1,17 +1,36 @@
 <template>
     <div class="wrapAll">
-        <div class="trophiesWrap">
-            <p class="tit">트로피</p>
-            <ul class="trophy">
-                <li>
-                    <img src="../assets/League/TrophyPl.png" alt="" width="130px">
-                    <div class="txt">
-                        <p class="count">13</p>
-                        <p class="Leaguename">프리미어 리그</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+        <p class="tit">트로피</p>
+        <ul class="trophiesWrap">
+            <li>
+                <div class="imgWrap">
+                    <img src="../assets/League/TrophyPl.png">
+                </div>
+                <div class="txt">
+                    <p class="count">13</p>
+                    <p class="leagueName">프리미어 리그</p>
+                </div>
+            </li>
+            <li>
+                <div class="imgWrap">
+                    <img src="../assets/League/TrophyFa.png" alt="">
+                </div>
+                <div class="txt">
+                    <p class="count">14</p>
+                    <p class="leagueName">FA 컵</p>
+                </div>
+            </li>
+            <li>
+                <div class="imgWrap">
+                    <img src="../assets/League/TrophyEfl.png" alt="">
+                </div>
+                <div class="txt">
+                    <p class="count">2</p>
+                    <p class="leagueName">EFL 컵</p>
+                </div>
+            </li>
+        </ul>
+        <ul class="historyWrap"></ul>
     </div>
 </template>
 
@@ -21,7 +40,14 @@
 
 <style scoped>
 .wrapAll {}
-.wrapAll .trophiesWrap {width: 1300px; margin: 0 auto;}
-.wrapAll .trophiesWrap .trophy {}
+.wrapAll .trophiesWrap {overflow: hidden;}
+.wrapAll .trophiesWrap > li {float: left; overflow: hidden; padding: 0 30px;}
+.wrapAll .trophiesWrap > li .txt {float: left;}
+.wrapAll .trophiesWrap > li .txt p {text-align: center; font-weight: 600;}
+.wrapAll .trophiesWrap > li .txt .count {font-size: 78px;}
+.wrapAll .trophiesWrap > li .txt .leagueName {font-size: 24px;}
 
+/* image */
+.wrapAll .trophiesWrap .imgWrap {float: left; width: 120px; height: 130px;}
+.wrapAll .trophiesWrap .imgWrap img {width: 100%; height: 100%;}
 </style>
